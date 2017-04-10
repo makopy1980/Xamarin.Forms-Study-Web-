7b483926253bca50a80027d256538e601482b095
makopy1980 <makopy.inside@gmail.com>
2017-04-10 12:54:14 +0900

    ベースプロジェクトの追加

---
+++ XFWebSample/XFWebSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace XFWebSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        // 外部ブラウザで開くボタンにバインド
+        private DelegateCommand _externalBrowserCommand;
+        public DelegateCommand ExternalBrowserCommand
+        {
+            get
+            {
+                return this._externalBrowserCommand = this._externalBrowserCommand ?? new DelegateCommand(ExternalBrowserCommandExecute);
+            }
+        }
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            
+        }
+
+        // ExternalBrowserCommand実行部
+        private void ExternalBrowserCommandExecute()
+        {
+            Device.OpenUri(new Uri("https://www.xamarin.com/"));
+        }
+    }
+}
+
